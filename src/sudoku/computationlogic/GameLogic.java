0ef8c532f610c33e3f18238642ff333a61bfd1d7
Mustafa Abdulrahman <1abdulrahmus@hdsb.ca>
2020-08-21 21:16:43 -0400

    start gamelogic

---
+++ src/sudoku/computationlogic/GameLogic.java
@@ -0,0 +1,20 @@
+package sudoku.computationlogic;
+
+import sudoku.constants.GameState;
+import sudoku.problemdomain.SudokuGame;
+
+public class GameLogic {
+
+    public static SudokuGame getNewGame() {
+        return new SudokuGame(
+                GameState.NEW,
+                GameGenerator.getNewGameGrid()
+        );
+    }
+
+    public static GameState checkForCompletion(int[][] grid) {
+        if (sudokuIsInvalid(grid)) return GameState.ACTIVE;
+        if (tilesAreNotFilled(grid)) return GameState.ACTIVE;
+        return GameState.COMPLETE;
+    }
+}
